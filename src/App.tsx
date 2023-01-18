f48b7df46b42c41ffb6064a6ee879ef66b8d9ece
Ivan Melnychuk <jacksmilereacher@gmail.com>
2023-01-18 08:28:00 +0200

    added corresponding color to li element in GoodList component

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,24 @@ import { GoodsList } from './GoodsList';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+      <button type="button" data-cy="all-button">
+        Load all goods
+      </button>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      <button type="button" data-cy="first-five-button">
+        Load 5 first goods
+      </button>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      <button type="button" data-cy="red-button">
+        Load red goods
+      </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <GoodsList goods={[]} />
+    </div>
+  );
+};
