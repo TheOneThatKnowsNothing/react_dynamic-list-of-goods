2cedd7e8024c531b5c7c2c73777fb568a56b9569
Ivan Melnychuk <jacksmilereacher@gmail.com>
2023-01-18 09:25:39 +0200

    added onClick handlers to buttons in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
-
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { Good } from './types/Good';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
 export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const handleRequest = (getGoods: Promise<Good[]>) => {
+    getGoods.then(setGoods);
+  };
+
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button type="button" data-cy="all-button">
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => handleRequest(getAll())}
+      >
         Load all goods
       </button>
 
-      <button type="button" data-cy="first-five-button">
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => handleRequest(get5First())}
+      >
         Load 5 first goods
       </button>
 
-      <button type="button" data-cy="red-button">
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => handleRequest(getRedGoods())}
+      >
         Load red goods
       </button>
 
-      <GoodsList goods={[]} />
+      <GoodsList goods={goods} />
     </div>
   );
 };
